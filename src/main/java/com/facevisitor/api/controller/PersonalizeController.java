bf49ca140c0e1e237b656dc181bb4eae9f057e05
허주영 <wndudpower@gmail.com>
2020-04-09 02:29:51 +0900

    Personalize Event Controller Mapping

--- src/main/java/com/facevisitor/api/controller/PersonalizeController.java
+++ src/main/java/com/facevisitor/api/controller/PersonalizeController.java
@@ -1,13 +1,11 @@
 package com.facevisitor.api.controller;
 
 import com.facevisitor.api.service.personalize.PersonalizeService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
@@ -25,5 +23,35 @@ public class PersonalizeController {
     @GetMapping("/ranking/{userId}")
     public ResponseEntity getRanking(@PathVariable Long userId) {
         return ResponseEntity.ok(personalizeService.getRankings(userId));
+    }
+
+    @PostMapping("{userId}/event/view/{itemId}")
+    public ResponseEntity viewEvent(@PathVariable Long userId, @PathVariable Long itemId) throws JsonProcessingException {
+        personalizeService.viewEvent(userId, itemId);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("{userId}/event/like/{itemId}")
+    public ResponseEntity likeEvent(@PathVariable Long userId, @PathVariable Long itemId) throws JsonProcessingException {
+        personalizeService.likeEvent(userId, itemId);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("{userId}/event/dislike/{itemId}")
+    public ResponseEntity dislikeEvent(@PathVariable Long userId, @PathVariable Long itemId) throws JsonProcessingException {
+        personalizeService.disLikeEvent(userId, itemId);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("{userId}/event/cart/{itemId}")
+    public ResponseEntity cartEvent(@PathVariable Long userId, @PathVariable Long itemId) throws JsonProcessingException {
+        personalizeService.cartEvent(userId, itemId);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("{userId}/event/order/{itemId}")
+    public ResponseEntity orderEvent(@PathVariable Long userId, @PathVariable Long itemId) throws JsonProcessingException {
+        personalizeService.orderEvent(userId, itemId);
+        return ResponseEntity.ok().build();
     }
 }
